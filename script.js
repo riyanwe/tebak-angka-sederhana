fd9d93813e7ea33ca883e5b712a8abbb583d18b5
riyanwe <rweliyanto34@gmail.com>
2022-10-08 12:58:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var lagi = true;
+while(lagi){
+// menangkap pilihan player
+let player = prompt('pilih angka 1 - 10');
+
+// menangkap pilihan komputer
+let comp = Math.floor(Math.random() * 10) + 1;
+
+let hasil;
+    // menentukkan rules
+        // benar
+        if (player == comp){
+            hasil = 'BENAR!';
+        } 
+        // terlalu rendah
+        else if (player < comp){
+            hasil= 'terlalu rendah';
+        } 
+        // terlalu tinggi
+        else if (player > comp){
+            hasil = 'terlalu tinggi';
+        } 
+        // jika player memasukkan angka asal atau selain angka
+        else{
+            hasil = 'SALAH! silahkan pilih angka 1 - 10';
+        };
+
+// menentukan hasil 
+alert('kamu memilih angka ' + player + ' dan komputer memilih angka ' + comp + ' pilihan kamu ' + hasil);
+
+// menanyakan main lagi atau tidak
+lagi = confirm('main lagi?');
+};
